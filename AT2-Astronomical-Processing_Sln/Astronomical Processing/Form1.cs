218bd9506b542a4216171b1d67981045ea70cc42
Niamh-30011705 <30011705@tafe.wa.edu.au>
2025-03-27 16:38:03 +1100

    Start

--- AT2-Astronomical-Processing_Sln/Astronomical Processing/Form1.cs
+++ AT2-Astronomical-Processing_Sln/Astronomical Processing/Form1.cs
@@ -2,7 +2,7 @@
 // Date: 27 March 2025
 // Version: 1.0
 // Astronomical Processing
-// Brief explanation of the program
+// A Forms based program to demonstrate the Binary Search and Bubble Sort.
 // List inputs, processes, outputs
 
 using System;
@@ -22,6 +22,22 @@ namespace Astronomical_Processing
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // Method for Get Data on button click
+        private void buttonGetData_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /* Binary Search. In this procedure, the entire list is divided into two sub-lists.
+         * If the item is found in the middle possition, it returns the location.
+         * Otherwise it jumps to either left or right sub-list and does the same process again until
+         * either the item is found or it exceeds the range.
+         */
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
