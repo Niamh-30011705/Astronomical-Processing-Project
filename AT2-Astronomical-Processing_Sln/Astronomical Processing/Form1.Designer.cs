f45bb06dcc682f247091676d4020c8285caf1f79
Niamh-30011705 <30011705@tafe.wa.edu.au>
2025-03-27 14:56:09 +1100

    Updates

---
+++ AT2-Astronomical-Processing_Sln/Astronomical Processing/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Astronomical_Processing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.buttonGetData = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputSearch = new System.Windows.Forms.TextBox();
+            this.inputEdit = new System.Windows.Forms.TextBox();
+            this.messageBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buttonQuit.Location = new System.Drawing.Point(172, 12);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(78, 45);
+            this.buttonQuit.TabIndex = 0;
+            this.buttonQuit.Text = "Quit";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            // 
+            // buttonGetData
+            // 
+            this.buttonGetData.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buttonGetData.Location = new System.Drawing.Point(256, 12);
+            this.buttonGetData.Name = "buttonGetData";
+            this.buttonGetData.Size = new System.Drawing.Size(78, 45);
+            this.buttonGetData.TabIndex = 1;
+            this.buttonGetData.Text = "Get Data";
+            this.buttonGetData.UseVisualStyleBackColor = true;
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buttonSort.Location = new System.Drawing.Point(340, 12);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(78, 45);
+            this.buttonSort.TabIndex = 2;
+            this.buttonSort.Text = "Sort Data";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buttonSearch.Location = new System.Drawing.Point(356, 63);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(62, 45);
+            this.buttonSearch.TabIndex = 3;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buttonEdit.Location = new System.Drawing.Point(356, 114);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(62, 45);
+            this.buttonEdit.TabIndex = 4;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Search Data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Edit Data";
+            // 
+            // inputSearch
+            // 
+            this.inputSearch.Location = new System.Drawing.Point(246, 76);
+            this.inputSearch.Name = "inputSearch";
+            this.inputSearch.Size = new System.Drawing.Size(104, 20);
+            this.inputSearch.TabIndex = 7;
+            // 
+            // inputEdit
+            // 
+            this.inputEdit.Location = new System.Drawing.Point(246, 127);
+            this.inputEdit.Name = "inputEdit";
+            this.inputEdit.Size = new System.Drawing.Size(104, 20);
+            this.inputEdit.TabIndex = 8;
+            // 
+            // messageBox
+            // 
+            this.messageBox.Location = new System.Drawing.Point(176, 165);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.ReadOnly = true;
+            this.messageBox.Size = new System.Drawing.Size(242, 20);
+            this.messageBox.TabIndex = 9;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(154, 186);
+            this.listBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 222);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.messageBox);
+            this.Controls.Add(this.inputEdit);
+            this.Controls.Add(this.inputSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonGetData);
+            this.Controls.Add(this.buttonQuit);
+            this.Name = "Form1";
+            this.Text = "Astronomical Processing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonQuit;
+        private System.Windows.Forms.Button buttonGetData;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputSearch;
+        private System.Windows.Forms.TextBox inputEdit;
+        private System.Windows.Forms.TextBox messageBox;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ AT2-Astronomical-Processing_Sln/Astronomical Processing/Form1.cs
@@ -0,0 +1,27 @@
+﻿// Niamh Cavanagh, Application Development Team, Sprint One
+// Date: 27 March 2025
+// Version: 1.0
+// Astronomical Processing
+// Brief explanation of the program
+// List inputs, processes, outputs
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Astronomical_Processing
+{
+    public partial class Form1: System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
